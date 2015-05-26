1290e6fc95d478c400537f622c5b91ac33d7204d
Mathew Brown <mathew.brown@gov.bc.ca>
2015-05-25 17:03:43 -0700

    added test data

--- restruct.py
+++ restruct.py
@@ -30,27 +30,28 @@ with open('test_hourly.csv', 'rb') as f:
     for sta_name, sta_data in dicte.iteritems():
          for column_name, column_values in sta_data.iteritems():
             # total = sum(column_values)
-            with open('newData.csv', "w") as output:
-                writer = csv.writer(output, lineterminator='\n')
-                writer.writerow(fields) # header
+            # with open('newData.csv', "w") as output:
+            #     writer = csv.writer(output, lineterminator='\n')
+            #     writer.writerow(fields) # header
 
                 #get total number of days
-                counter = 0
-                if column_name == 'date_time':
-                    for j, tm in enumerate(column_values):
-                        if j == 0:
-                            d = datetime.strptime(tm, "%Y-%m-%d %H:%M:%S").date()
-                            writer.writerow([str(d),])
+            counter = 0
+            if column_name == 'date_time':
+                for j, tm in enumerate(column_values):
+                    if j == 0:
+                        d = datetime.strptime(tm, "%Y-%m-%d %H:%M:%S").date()
+                        writer.writerow([str(d),])
 
-                        d1 = datetime.strptime(tm, "%Y-%m-%d %H:%M:%S").date()
-                        if d1 == d:
-                            counter += 1
+                    d1 = datetime.strptime(tm, "%Y-%m-%d %H:%M:%S").date()
+                    if d1 == d:
+                        counter += 1
 
-                        else: # new day, write average value for previous day for all columns.
+                    else: # new day, write average value for previous day for all columns.
 
-                            writer.writerow([column_values[:counter]])
-                        print('days '+str(count))
-                    print(column_name)
+                        # writer.writerow([column_values[:counter]])
+                    # print('days '+str(count))
+            print(column_name)
+            print(counter)
             # for column_name, column_values in sorted(sta_data):
         #     total = sum(column_values)
 
